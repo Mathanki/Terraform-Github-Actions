ad062ae4c1e248111fe4a3e42021a6292aeca231
mathanki <mathanki091suren@gmail.com>
2025-12-31 14:46:20 -0500

    first commit

---
+++ code/backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+   backend "s3" {
+    bucket         = "tech-demo-mathanki-terraform-state"
+    key            = "dev/terraform-day27.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
